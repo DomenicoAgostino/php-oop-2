db9ad34017c2528f1494371cf8ea08b37aa3416c
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-06-20 16:08:28 +0200

    Inizio esercizio

---
+++ store.php
@@ -0,0 +1,21 @@
+<?php
+
+class Product{
+  private $name;
+  private $price;
+  private $code;
+
+  public function __construct($_name,
+                              $_price,
+                              $_code){
+      $this->name =  ;
+      $this->price = ;
+      $this->code = ;
+  }
+
+  public function set
+
+}
+
+
+?>
